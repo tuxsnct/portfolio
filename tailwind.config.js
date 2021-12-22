bb8e15de62481e8cb81b1de854614fa7b9b17355
Hideki Tanaka <work@tuxsnct.com>
2021-12-22 17:19:06 +0900

    change the file structure and add dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  plugins: [],
+  purge: [
+    './index.html',
+    './src/**/*.{ts,tsx}'
+  ],
+  theme: {},
+  variants: {}
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
-import preactPlugin from '@preact/preset-vite'
+import reactPlugin from '@vitejs/plugin-react'
 import { UserConfig } from 'vite'
+import { VitePWA as vitePwaPlugin } from 'vite-plugin-pwa'
 
 const config: UserConfig = {
-  base: './',
-  plugins: [preactPlugin()]
+  plugins: [
+    reactPlugin({
+      babel: {
+        plugins: [
+          'babel-plugin-macros',
+          [
+            '@emotion/babel-plugin-jsx-pragmatic',
+            {
+              export: 'jsx',
+              import: '__cssprop',
+              module: '@emotion/react'
+            }
+          ],
+          [
+            '@babel/plugin-transform-react-jsx',
+            { pragma: '__cssprop' },
+            'twin.macro'
+          ]
+        ]
+      }
+    }),
+    vitePwaPlugin({
+      registerType: 'autoUpdate',
+      workbox: {
+        inlineWorkboxRuntime: true
+      }
+    })
+  ]
 }
 
 export default config
