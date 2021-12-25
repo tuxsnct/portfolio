7dccf04f49cbaee694bfb058f3d1193a7df5fcea
Hideki Tanaka <work@tuxsnct.com>
2021-12-25 22:48:35 +0900

    remove recaptcha

--- components/app.tsx
+++ components/app.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 import tw, { GlobalStyles } from 'twin.macro'
 import { Provider } from 'unistore/react'
 import { Route, Switch } from 'wouter'
@@ -9,19 +8,12 @@ import { HomePage, NotFoundPage } from '../pages'
 const App: React.FC = () => (
   <>
     <GlobalStyles />
-    <GoogleReCaptchaProvider
-      reCaptchaKey={import.meta.env.VITE_RECAPTCHA_KEY}
-      scriptProps={{
-        async: true
-      }}
-    >
-      <Provider store={store}>
-        <Switch>
-          <Route path="/" component={HomePage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </Provider>
-    </GoogleReCaptchaProvider>
+    <Provider store={store}>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Provider>
   </>
 )
 
