6b762868824e7c58baac42b72038b7626db33db0
Hideki Tanaka <work@tuxsnct.com>
2021-12-26 00:01:10 +0900

    fix registration method for service worker

--- components/app.tsx
+++ components/app.tsx
@@ -2,26 +2,21 @@ import React from 'react'
 import tw, { GlobalStyles } from 'twin.macro'
 import { Provider } from 'unistore/react'
 // eslint-disable-next-line import/no-unresolved
-import { useRegisterSW } from 'virtual:pwa-register/react'
 import { Route, Switch } from 'wouter'
 import { store } from '../libraries'
 import { HomePage, NotFoundPage } from '../pages'
 
-const App: React.FC = () => {
-  useRegisterSW()
-
-  return (
-    <>
-      <GlobalStyles />
-      <Provider store={store}>
-        <Switch>
-          <Route path="/" component={HomePage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </Provider>
-    </>
-  )
-}
+const App: React.FC = () => (
+  <>
+    <GlobalStyles />
+    <Provider store={store}>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Provider>
+  </>
+)
 
 export {
   App
--- index.tsx
+++ index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
+// eslint-disable-next-line import/no-unresolved
+import { registerSW } from 'virtual:pwa-register'
 import { App } from './components'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/700.css'
+
+registerSW()
 
 render(<App />, document.body)
 
