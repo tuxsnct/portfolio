cc9e4613aa1695375732541f7aadd11bf215c99a
Hideki Tanaka <work@tuxsnct.com>
2021-12-26 11:12:34 +0900

    remove unnecessary props

--- layouts/base.tsx
+++ layouts/base.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import tw from 'twin.macro'
 import { Footer, SEO } from '../components'
 
-type BaseLayoutProperties = React.ComponentProps<typeof SEO> & {
-  mainProps?: React.ComponentProps<React.ReactHTML['main']>
-}
+type BaseLayoutProperties = React.ComponentProps<typeof SEO>
 
-const BaseLayout: React.FC<BaseLayoutProperties> = ({ children, mainProps, ...rest }) => (
+const BaseLayout: React.FC<BaseLayoutProperties> = ({ children, ...rest }) => (
   <div tw="w-full h-full min-w-full min-h-screen flex flex-col bg-slate-100 dark:bg-slate-900 overflow-hidden">
     <SEO {...rest} />
     {children}
